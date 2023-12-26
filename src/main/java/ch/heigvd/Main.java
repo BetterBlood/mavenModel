a75fc575f1519c7280785871124d17d0118a9d39
Better_Blood <45463619+BetterBlood@users.noreply.github.com>
2023-12-26 15:56:37 +0100

    first commit

---
+++ src/main/java/ch/heigvd/Main.java
@@ -0,0 +1,12 @@
+package ch.heigvd;
+
+import static java.lang.System.exit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+
+        exit(0);
+    }
+}
